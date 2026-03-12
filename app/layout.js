80d142200266060d8b54d34d52386914c9de58ea
JoaoMaia <joaocarlosbgm@gmail.com>
2026-03-12 12:03:46 -0300

    Update OG image

--- app/layout.js
+++ app/layout.js
@@ -8,17 +8,24 @@ const ibmPlexMono = IBM_Plex_Mono({
 });
 
 export const metadata = {
-    title: { default: 'Vinil89', template: '%s | Vinil89' },
-    description: 'Banda de pop/rock de Recife',
-    openGraph: {
-      title: 'Vinil89',
-      description: 'Banda de pop/rock de Recife',
-      url: 'https://vinil89.vercel.app/',
-      images: [{ url: '/og-image.png', width: 1200, height: 630 }],
-      type: 'website',
-    },
-    icons: { icon: '/favicon.ico', apple: '/apple-touch-icon.png' },
-  };
+  metadataBase: new URL("https://vinil89.vercel.app"),
+  title: { default: "Vinil89", template: "%s | Vinil89" },
+  description: "Banda de pop/rock de Recife",
+  openGraph: {
+    title: "Vinil89",
+    description: "Banda de pop/rock de Recife",
+    url: "https://vinil89.vercel.app",
+    images: [
+      {
+        url: "https://vinil89.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  icons: { icon: "/favicon.ico", apple: "/apple-touch-icon.png" },
+};
 
 export default function RootLayout({ children }) {
   return (
