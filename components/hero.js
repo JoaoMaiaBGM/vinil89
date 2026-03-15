1de4186193bced6a894cdb979809ec63d28233a0
JoaoMaia <joaocarlosbgm@gmail.com>
2026-03-14 23:00:55 -0300

    Fix mobile hero bg

--- components/hero.js
+++ components/hero.js
@@ -2,13 +2,21 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <section className="relative h-[60svh] w-full bg-black md:h-[95svh] lg:h-screen flex flex-col items-center justify-center">
+    <section className="relative h-[48svh] w-full bg-black md:h-[70svh] lg:h-[88svh] flex flex-col items-center justify-center">
       <Image
         src="https://static.wixstatic.com/media/c3fe9c_64a2f466993247fb86b18a52d833441b~mv2.jpg"
-        alt="Vinil89 hero background"
+        alt="Vinil89 desktop hero background"
         fill
         priority
-        className="object-cover object-top opacity-70"
+        className="object-cover object-top opacity-70 hidden"
+      />
+
+      <Image
+        src="https://static.wixstatic.com/media/c3fe9c_b884a49de973496fb4796d067aacb732~mv2.jpg"
+        alt="Vinil89 mobile hero background"
+        fill
+        priority
+        className="object-cover object-top opacity-70 md:block"
       />
 
       <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center px-4">
@@ -17,7 +25,7 @@ export default function Hero() {
           alt="Vinil89 logo text"
           width={400}
           height={400}
-          className="flash-logo w-60 object-contain mt-20 md:w-96 md:mt-50"
+          className="flash-logo w-60 object-contain md:w-96 xl:mt-60"
         />
 
         <a href="/music-videos">
