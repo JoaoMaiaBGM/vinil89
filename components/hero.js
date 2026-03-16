6c504040302c8cbb7dc9ce09da45b656c512a20b
JoaoMaia <joaocarlosbgm@gmail.com>
2026-03-15 21:54:46 -0300

    Fix hero background image

--- components/hero.js
+++ components/hero.js
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <section className="relative h-[55svh] w-full bg-black md:h-[70svh] lg:h-[88svh] flex flex-col items-center justify-center">
+    <section className="relative h-[70svh] w-full bg-black md:h-screen lg:h-[150svh] flex flex-col items-center justify-center">
       <Image
         src="https://static.wixstatic.com/media/c3fe9c_64a2f466993247fb86b18a52d833441b~mv2.jpg"
         alt="Vinil89 desktop hero background"
         fill
         priority
-        className="object-cover object-top opacity-70 hidden"
+        className="object-cover object-top opacity-70 hidden md:block"
       />
 
       <Image
@@ -16,7 +16,7 @@ export default function Hero() {
         alt="Vinil89 mobile hero background"
         fill
         priority
-        className="object-cover object-top opacity-70 md:block"
+        className="object-cover object-top opacity-70 block md:hidden"
       />
 
       <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center px-4">
@@ -25,13 +25,13 @@ export default function Hero() {
           alt="Vinil89 logo text"
           width={400}
           height={400}
-          className="flash-logo w-60 object-contain md:w-96 xl:mt-60"
+          className="flash-logo w-60 object-contain md:w-96"
         />
 
         <a href="/music-videos">
           <button
             type="button"
-            className="bg-[#999999] text-white border border-white px-4 py-2 rounded-md cursor-pointer mt-5 hover:bg-black hover:border-black hover:text-white"
+            className="bg-[#999999] text-white border uppercase border-white px-4 py-2 rounded-md cursor-pointer mt-5 hover:bg-black hover:border-black hover:text-white"
           >
             Ouça agora
           </button>
