65e9c7b9f1f56f4bf09addee71cafcda8124e5e9
JoaoMaiaBGM <joaocarlosbgm@gmail.com>
2026-07-07 19:01:33 -0300

    Upadate caroussel images and fix next and previous action

--- components/media/_components/gallery.js
+++ components/media/_components/gallery.js
@@ -2,7 +2,13 @@ import Image from 'next/image';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 export default function Gallery({ images, currentImageIndex, setCurrentImageIndex }) {
-  console.log(images[currentImageIndex].src);
+  const goToPrevious = () => {
+    setCurrentImageIndex((currentImageIndex - 1 + images.length) % images.length);
+  };
+
+  const goToNext = () => {
+    setCurrentImageIndex((currentImageIndex + 1) % images.length);
+  };
 
   return (
     <>
@@ -15,27 +21,24 @@ export default function Gallery({ images, currentImageIndex, setCurrentImageInde
           className="h-96 w-full object-cover object-top"
         />
 
-        {/* Navigation Buttons */}
         <button
-          onClick={() => setCurrentImageIndex(currentImageIndex - 1)}
+          onClick={goToPrevious}
           className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-primary/80 hover:bg-primary text-primary-foreground p-2 rounded-full transition-colors z-10"
         >
           <FaChevronLeft className="w-6 h-6" />
         </button>
         <button
-          onClick={() => setCurrentImageIndex(currentImageIndex + 1)}
+          onClick={goToNext}
           className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-primary/80 hover:bg-primary text-primary-foreground p-2 rounded-full transition-colors z-10"
         >
           <FaChevronRight className="w-6 h-6" />
         </button>
 
-        {/* Image Counter */}
         <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-background/80 px-4 py-2 rounded-full text-sm text-foreground">
           {currentImageIndex + 1} / {images.length}
         </div>
       </div>
 
-      {/* Thumbnail Gallery */}
       <div className="grid grid-cols-4 gap-2 mt-4">
         {images.map((img, idx) => (
           <button
